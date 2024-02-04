d46f4297fbb9095b871dd27b0b88718bb6477c81
Sam-Geboers-private <157908436+Sam-Geboers-private@users.noreply.github.com>
2024-02-04 21:19:37 +0100

    authentication page finished

--- src/Components/sign-up-form/sign-up-form.component.jsx
+++ src/Components/sign-up-form/sign-up-form.component.jsx
@@ -21,11 +21,9 @@ const SignUpForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields);
 	const { displayName, email, password, confirmPassword } = formFields;
 
-	console.log(formFields);
-
 	const resetFormFields = (à => {
 		setFormFields(defaultFormFields);
-	})
+	});
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
@@ -61,47 +59,47 @@ const SignUpForm = () => {
 
   return (
     <div className='sign-up-container'>
-			<h2>I do not have a account</h2>
+		<h2>I do not have a account</h2>
       <span>Sign up with your email and password</span>
-			<form onSubmit={handleSubmit}>
-				<FormInput 
-					label="Display Name"
-					type="text" 
-					required 
-					onChange={handleChange} 
-					name='displayName' 
-					value={displayName} 
-				/>
+		<form onSubmit={handleSubmit}>
+			<FormInput 
+				label="Display Name"
+				type="text" 
+				required 
+				onChange={handleChange} 
+				name='displayName' 
+				value={displayName} 
+			/>
 
-				<FormInput 
-					label="Email"
-					type="email" 
-					required 
-					onChange={handleChange} 
-					name='email' 
-					value={email} 
-				/>
+			<FormInput 
+				label="Email"
+				type="email" 
+				required 
+				onChange={handleChange} 
+				name='email' 
+				value={email} 
+			/>
 
-				<FormInput 
-					label="Password"
-					type="password" 
-					required 
-					onChange={handleChange} 
-					name='password' 
-					value={password} 
-				/>
+			<FormInput 
+				label="Password"
+				type="password" 
+				required 
+				onChange={handleChange} 
+				name='password' 
+				value={password} 
+			/>
 
-				<FormInput 
-					label="Confirm Password"
-					type="password" 
-					required 
-					onChange={handleChange} 
-					name='confirmPassword' 
-					value={confirmPassword} 
-				/>
+			<FormInput 
+				label="Confirm Password"
+				type="password" 
+				required 
+				onChange={handleChange} 
+				name='confirmPassword' 
+				value={confirmPassword} 
+			/>
 
-				<Button type="submit">Sign Up</Button>
-			</form>
+			<Button type="submit">Sign Up</Button>
+		</form>
     </div>
   );
 };
