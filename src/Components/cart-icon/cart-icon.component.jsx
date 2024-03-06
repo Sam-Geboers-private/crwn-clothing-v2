bf77b39a65e445f47764277436ba03c937c8a848
Sam-Geboers-private <157908436+Sam-Geboers-private@users.noreply.github.com>
2024-03-06 21:41:49 +0100

    extra

--- src/Components/cart-icon/cart-icon.component.jsx
+++ src/Components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,5 @@
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 import { useContext } from 'react';
 
